47a1ba1c1689079e366955c11e86e1af9819d588
iamArnon <arnon.pkt.teen@gmail.com>
2023-11-07 22:59:27 +0700

    update

--- index.js
+++ index.js
@@ -16,31 +16,13 @@ app.get('/', (req, res) => {
     res.sendFile(join(__dirname, '/public/index.html'));
   });
 
-  // io.on('connection', (socket) => {
-  //   console.log('a user connected');
-  //   socket.on('disconnect', () => {
-  //       console.log('user disconnected');
-  //     });
-
-    // socket.on('chat message', (msg) => {
-    //     console.log('message: ' + msg);
-    //     io.emit('chat message', msg);
-
-    // });
-
-  // });
+  
 
   io.on("connection", (socket) => {
-    socket.on("newuser",(username)=>{
-      socket.broadcast.emit("update",username + "joined")
-      console.log(username + " has joined");
-      socket.on('disconnect', () => {
-              console.log(username + ' user disconnected');
-            });
-    })
 
     socket.on('chat', (message) => {
-      console.log('message: ' + message);
+      const data = JSON.parse(message)
+      console.log('username ' + data.username +  ' message: '  + data.message);
       io.emit('chat', message);
 
   });
